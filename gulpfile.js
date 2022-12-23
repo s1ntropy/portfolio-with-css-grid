61695e42a2b8fe7fdc946d520126186bcf46cc62
Nate Hughey <jonathan.hughey@gmail.com>
2022-12-23 16:23:59 -0600

    Update gulp version and syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
-const gulp = require('gulp'),
 browserSync = require('browser-sync').create(),
 sass = require('gulp-sass');
 
+const gulp = require('gulp')
+const { watch, series } = require('gulp');
 
-// Static server & watch scss + html files
-gulp.task('watch', ['sass'], function() {
+function init() {
+    browserSync.init({
+        server: '.'
+        });
+}
 
-browserSync.init({
-server: '.'
-});
+function reload() {
+    browserSync.reload();
+}
 
-gulp.watch('./scss/**/*.scss', ['sass'], browserSync.reload);
-gulp.watch('./*.html').on('change', browserSync.reload);
-gulp.watch('./js/**/*.js', browserSync.reload);
+function sass() {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+}
 
-});
+exports.default = function() {
+  init();
+  // You can use a single task
+  watch('./scss/**/*.scss', series(sass, reload));
+  // Or a composed task
+  watch('./*.html', series(reload));
+  watch('./js/**/*.js', series(reload))
+};
 
-// Compile Sass into CSS & inject into browsers
-gulp.task('sass', function() {
-return gulp.src('./scss/**/*.scss')
-.pipe(sass().on('error', sass.logError))
-.pipe(gulp.dest('./css'))
-.pipe(browserSync.stream());
-});
-
-
-// default will also watch
-gulp.task('default', ['watch']);
 
